a18f764bedb5c11e453d2fb1956ba08d3655a0a4
2017-A-15 <ea_pain@yahoo.com>
2020-11-30 17:25:27 +0330

    move refs

--- network.py
+++ network.py
@@ -1,7 +1,1 @@
 
-
-
-# https://medium.com/@pasdan/genetic-algorithm-the-travelling-salesman-problem-via-python-deap-f238e0dd1a73
-# https://towardsdatascience.com/evolution-of-a-salesman-a-complete-genetic-algorithm-tutorial-for-python-6fe5d2b3ca35
-# https://github.com/ahmedfgad/GeneticAlgorithmPython
-# https://github.com/gmichaelson/GA_in_python/blob/master/GA%20example.ipynb
